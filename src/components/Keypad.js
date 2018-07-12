a55f2ac72448052976e5580c7c5c49a411118a6b
James Ferguson <ncjamesferguson@gmail.com>
2018-07-12 00:10:18 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,9 +5,13 @@ export default class Keypad extends React.Component {
   render() {
     return (
       <div className="keypad">
-      
+      <input type="password" onKeyUp={this.entering} ></input>
       </div>
     )
+
+    entering() {
+      console.log("Entering password...")
+    }
   }
 
 }
